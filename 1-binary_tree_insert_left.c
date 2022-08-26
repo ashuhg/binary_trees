0c3c39320cfcaa04c0f37698d7d018b7bebbad7e
ashuhg <ashenafihagos.ah@gmail.com>
2022-08-26 05:42:07 +0300

    binary trees and algos

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,6 +1,14 @@
 #include "binary_trees.h"
 
-
+/**
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ * @parent: pointer to the node to insert the left-child in
+ * @value: value to store in the new node
+ *
+ * Return: Pointer to the newly created node
+ *         NULL on failure
+ *         NULL if parent is NULL
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,6 +1,10 @@
 #include "binary_trees.h"
 
-
+/**
+ * binary_tree_preorder - goes through a binary tree using pre-order traversal
+ * @tree: tree to traverse
+ * @func: pointer to a function to call for each node
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
